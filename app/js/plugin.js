aca14a80bce7ddb1c4da5a8d40dde7eec03a620e
prostonastya <find@li.ru>
2017-02-09 15:51:39 +0200

    дз на 10.02. сверстала страницу, сделала форму поиска

--- app/js/plugin.js
+++ app/js/plugin.js
@@ -1,4 +1,5 @@
 ;(function ($) {
+  // open second form
    var moreOptionBtn = $('.more-option');
    var secondForm = $('.second-form');
 
@@ -6,4 +7,26 @@
        secondForm.slideToggle();
        moreOptionBtn.toggleClass('open');
    });
+
+// init slick slider
+$('.areas-slick-js').slick({
+  slidesToShow: 7,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 2000,
+  // centerMode: true,
+});
+
+// open search-form
+  var serchIcon = $('.serch-ic');
+  var searchForm = $('.search-form');
+  serchIcon.on('click', function (){
+  // searchForm.slideToggle(1000);
+  searchForm.animate({width:'toggle'}, 1000);
+  serchIcon.toggleClass('open');
+      
+   });
+ 
+
+
 })(jQuery);
