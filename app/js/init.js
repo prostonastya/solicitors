f21e696e75623ddb4dd232cca9d25a2f79e61a99
prostonastya <find@li.ru>
2017-02-14 00:14:10 +0200

    не сделан поиск в бургере и чекбоксы

--- app/js/init.js
+++ app/js/init.js
@@ -1,4 +1,11 @@
 ;(function ($){
 // init formstyler
 $('select').styler();
+
+// init slicknav
+$('.header-nav').slicknav({
+	label: '',
+	prependTo:'.header .container',
+});
+
 })(jQuery);
--- app/js/plugin.js
+++ app/js/plugin.js
@@ -15,6 +15,26 @@ $('.areas-slick-js').slick({
   autoplay: false,
   autoplaySpeed: 2000,
   // centerMode: true,
+  responsive:[
+    {
+      breakpoint: 1024,
+      settings: {
+      slidesToShow: 4,
+       }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+      slidesToShow: 2,
+       }
+    },
+    {
+      breakpoint: 479,
+      settings: {
+      slidesToShow: 1,
+       }
+    }
+  ]
 });
 
 // open search-form
@@ -22,7 +42,7 @@ $('.areas-slick-js').slick({
   var searchForm = $('.search-form');
   serchIcon.on('click', function (){
   // searchForm.slideToggle(1000);
-  searchForm.animate({width:'toggle'}, 1000);
+  searchForm.animate({width:'toggle'}, 500);
   serchIcon.toggleClass('open');
       
    });
